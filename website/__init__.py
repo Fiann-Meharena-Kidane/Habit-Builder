de930237074675beafa55f6996f43b46d5ccf95e
finan <finanmeharenakidane@gmail.com>
2022-09-03 20:31:16 +0400

    added loggin and register templates created authentication functions, final touch before builidng relation between user, challenge, habit and days, Arriverdci!

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
-
+from flask_login import LoginManager, login_manager
 db = SQLAlchemy()
 DB_NAME = 'challenges.db'
 
@@ -16,9 +15,18 @@ def create_app():
 
     from .views import views
     from .auth import auth
+    from .models import User
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    login_manager=LoginManager()
+    login_manager.login_view='view.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,51 @@
 from flask import Blueprint, render_template, redirect, request, flash, url_for
 from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import *
+
+auth = Blueprint('auth', __name__)
 
 
-auth=Blueprint('auth', __name__)
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not User.query.filter_by(email=email).first():
+            hashed_password=generate_password_hash(password, method='sha256')
+            new_user = User(
+                name=name,
+                email=email,
+                password=hashed_password
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            # return 'hello'
+            return redirect(url_for('views.home'))
+        else:
+            return 'email exists, login'
+    else:
+        return render_template('register.html')
 
 
-@auth.route('/')
+@auth.route('/', methods=['POST','GET'])
 def login():
-    return render_template('base.html')
+    if request.method=='POST':
+        email=request.form.get('email')
+        user=User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password,request.form.get('password')):
+                return redirect(url_for('views.home'))
+            else:
+                return 'wrong password'
+        else:
+            return 'email doesnt exist'
+
+    else:
+        return render_template('login.html')
